67071a5435eb7625053a4bf389c10bccef8cfa72
leafdesk <lsg001008@gmail.com>
2024-11-12 12:49:22 +0900

    :bulb: COMMENT: `LogUtil` 주석 스타일 변경 - 3줄

--- lib/utils/log_util.dart
+++ lib/utils/log_util.dart
@@ -3,22 +3,30 @@ import 'package:logger/logger.dart';
 class LogUtil {
   static final Logger _logger = Logger();
 
-  // Debug 로그
+  /// Debug 로그.
+  ///
+  ///
   static void d(String tag, String message) {
     _logger.d("[$tag] $message");
   }
 
-  // Info 로그
+  /// Info 로그.
+  ///
+  ///
   static void i(String tag, String message) {
     _logger.i("[$tag] $message");
   }
 
-  // Warning 로그
+  /// Warning 로그.
+  ///
+  ///
   static void w(String tag, String message) {
     _logger.w("[$tag] $message");
   }
 
-  // Error 로그
+  /// Error 로그.
+  ///
+  ///
   static void e(String tag, String message) {
     _logger.e("[$tag] $message");
   }
